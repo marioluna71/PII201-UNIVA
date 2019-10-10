a6e053b3969841b340473238f958e8ca54a9bff0
marioluna71 <51181989+marioluna71@users.noreply.github.com>
2019-10-09 21:04:12 -0500

    Clase 5 php

---
+++ php/variables.php
@@ -0,0 +1,42 @@
+<?php
+
+    $variableOne = 100;
+    $variableTwo = 200;
+    $result;
+
+    //add
+    $result = $variableOne + $variableTwo;
+
+    //sust
+    //$result = $variableOne - $variableTwo;
+
+    //mult
+    //$result = $variableOne * $variableTwo;
+
+    //divi
+    //$result = $variableOne / $variableTwo;
+
+    //para imprimir echo o print
+
+    print $result;
+    //print "<br>"; //salto de línea
+
+
+    //if ($result == 300) {
+       //print "Correct";
+    //}
+
+    //if ($result > 100){
+        //print "This is bigger than 100";
+    //} else {
+        //ptint "this is lower than 100";
+    //}
+    
+    $firstName = "Mario";
+    $lastName = "Luna";
+
+    $fullName = $firstName." ".$lastName;
+
+    print $fullName;
+
+?>
